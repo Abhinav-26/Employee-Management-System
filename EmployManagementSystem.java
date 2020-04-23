33944c62edda5ec4f2d857014fb331997fb37980
Abhinav Dubey <abhinavdubey616@gmail.com>
2020-04-24 00:17:00 +0530

    Program file to run

--- EmployManagementSystem.java
+++ EmployManagementSystem.java
@@ -10,12 +10,12 @@ class MainMenu
 {
   public void menu()
   {
-    System.out.println("*******************************************");
-    System.out.println("\t  EMPLOYEE MANAGEMENT SYSTEM");
-    System.out.println("*******************************************");
-    System.out.println("\t\t\t--------------------");
-    System.out.println("\t\t\t ~$ Abhinav Dubey");
-    System.out.println("\t\t\t--------------------");
+    System.out.println("\t\t*******************************************");
+    System.out.println("\t\t\t  EMPLOYEE MANAGEMENT SYSTEM");
+    System.out.println("\t\t*******************************************");
+    System.out.println("\t\t\t    --------------------");
+    System.out.println("\t\t\t     ~$ Abhinav Dubey");
+    System.out.println("\t\t\t    --------------------");
     System.out.println("\n\nPress 1 : To Add an Employee Details");
     System.out.println("Press 2 : To See an Employee Details ");
     System.out.println("Press 3 : To Remove an Employee");
